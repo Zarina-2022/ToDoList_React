4f669b3bee98398971b818a28ab2f863e9c74e20
Zarina-2022 <sekerdagzarina@gmail.com>
2023-01-12 14:00:34 +0100

    done or not done button functions have been written

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,9 +1,9 @@
 import React from "react"
 
-const TodoList = ({item, yapilacaklar, setYapilacaklar})=>{
-  // silme islemi
-  const handleDelete=()=>{
-    const temp = yapilacaklar.filter((x)=>x.id !== item.id)       // temp=temporary=gecici
+const TodoList = ({ item, yapilacaklar, setYapilacaklar }) => {
+  // Button Delete
+  const deleteBtn = () => {
+    const temp = yapilacaklar.filter((x) => x.id !== item.id)       // temp=temporary=gecici
     setYapilacaklar(temp)
     /*
     // for dongusu ile silme islemi:
@@ -18,35 +18,60 @@ const TodoList = ({item, yapilacaklar, setYapilacaklar})=>{
     }
     setYapilacaklar(temp)
     */
-   /*
-    // ustteki for dondusunu kisaca yazmak icin:
-        const temp = []
-    for(let i=0, i<yapilacaklar.length, i++){
-      if(item.id !== yapilacaklar[i].id){
-        temp.push(yapilacaklar[i])
+    /*
+     // ustteki for dondusunu kisaca yazmak icin:
+         const temp = []
+     for(let i=0, i<yapilacaklar.length, i++){
+       if(item.id !== yapilacaklar[i].id){
+         temp.push(yapilacaklar[i])
+       }
+     }
+     setYapilacaklar(temp)
+    */
+  }
+  // done or not done button
+  const doneNotDoneBtn = ()=>{
+    var temp = []
+    for(var a=0; a<yapilacaklar.length; a++){
+      if(yapilacaklar[a].id === item.id){
+        var newDoneNotDoneList={
+          // spread ile newDoneNotDoneList object'i daha kisa write:
+           ...item,
+            haveDone: !item.haveDone // burda yeni item da ekleyebiliriz, veya var olani change.
+          /* burda daha genis write:
+          id:item.id,
+          text:item.text,
+          date:item.date,
+          haveDone: !item.haveDone // false'u true yapacak
+          */
+        }
+        temp.push(newDoneNotDoneList)
+      } else{
+        temp.push(yapilacaklar[a])
       }
     }
     setYapilacaklar(temp)
-   */
   }
-    return(
-        <div className="alert alert-secondary d-flex justify-content-between">
-                  
-                  <div>
-                    <h2>{item.text}</h2>
-                    <p>Date: {new Date(item.date).toLocaleString().replaceAll("/",".")}</p>
-                  </div>
 
-                  <div className="d-flex align-items-center">
-                    <div class="btn-group" role="group" aria-label="Basic example">
-                      <button type="button" class="btn btn-sm btn-success">{item.done === true ? "Not Done" : "Done"}</button>
-                      <button type="button" class="btn btn-sm btn-warning">Update</button>
-                      <button onClick={handleDelete} type="button" class="btn btn-sm btn-danger">Delete</button>
-                    </div>
-                  </div>
 
-                </div>
-    )
+  return (
+    <div className="alert alert-secondary d-flex justify-content-between">
+
+      <div>
+        <h2 style={{textDecoration: item.haveDone === true ? "line-through" : "none"}}>{item.text}</h2>
+        <p>Date: {new Date(item.date).toLocaleString().replaceAll("/", ".")}</p>
+      </div>
+
+      <div className="d-flex align-items-center">
+        <div class="btn-group" role="group" aria-label="Basic example">
+          <button onClick={doneNotDoneBtn} type="button" class="btn btn-sm btn-success">{item.haveDone === true ? "Not Done" : "Done"}</button>
+          <button type="button" class="btn btn-sm btn-warning">Update</button>
+          <button onClick={deleteBtn} type="button" class="btn btn-sm btn-danger">Delete</button>
+        </div>
+      </div>
+
+    </div>
+  )
 }
 
 
